12aff0f26715b35eb1c199604365625be7e0d420
iKramp <nejc.susnik05@gmail.com>
2022-03-20 17:08:45 +0100

    idk

--- structure_encoder.cpp
+++ structure_encoder.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-enum class blockType { NOTHING = -1, AIR, DIRT, STONE_BLOCK, GRASS_BLOCK, STONE, WOOD, LEAVES, SAND, SNOWY_GRASS_BLOCK, SNOW_BLOCK, ICE };
+enum class blockType { NOTHING, AIR, DIRT, STONE_BLOCK, GRASS_BLOCK, STONE, WOOD, LEAVES, CANOPY, BRANCH, SAND, SNOWY_GRASS_BLOCK, SNOW_BLOCK, ICE_BLOCK, IRON_ORE, COPPER_ORE, GRASS, TORCH, CACTUS};
 
 struct structure {
     string name;
@@ -22,58 +22,107 @@ struct structure {
 void createDataBlock() {
     vector<structure> structures;
 
-    structures.push_back(structure("Tree", 5, 12, { blockType::NOTHING, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::NOTHING,
-                                                    blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES,
-                                                    blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES,
-                                                    blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES,
-                                                    blockType::NOTHING, blockType::LEAVES, blockType::LEAVES, blockType::LEAVES, blockType::NOTHING,
-                                                    blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
-                                                    blockType::LEAVES, blockType::WOOD, blockType::WOOD, blockType::AIR, blockType::NOTHING,
-                                                    blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
-                                                    blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::WOOD, blockType::LEAVES,
-                                                    blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
-                                                    blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
-                                                    blockType::AIR, blockType::WOOD, blockType::WOOD, blockType::WOOD, blockType::AIR,
+    structures.push_back(structure("tree_1", 5, 12, {
+            blockType::CANOPY, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::LEAVES, blockType::BRANCH, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::BRANCH, blockType::LEAVES,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::AIR, blockType::WOOD, blockType::WOOD, blockType::WOOD, blockType::AIR,
+    }));
+
+    structures.push_back(structure("tree_0", 5, 12, {
+            blockType::CANOPY, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING, blockType::NOTHING,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::AIR, blockType::AIR, blockType::WOOD, blockType::BRANCH, blockType::LEAVES,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::LEAVES, blockType::BRANCH, blockType::WOOD, blockType::AIR, blockType::AIR,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::NOTHING, blockType::AIR, blockType::WOOD, blockType::AIR, blockType::NOTHING,
+            blockType::AIR, blockType::WOOD, blockType::WOOD, blockType::WOOD, blockType::AIR,
+    }));
+
+    structures.push_back(structure("cactus_0", 2, 5, {
+            blockType::NOTHING, blockType::CACTUS,
+            blockType::CACTUS, blockType::CACTUS,
+            blockType::CACTUS, blockType::CACTUS,
+            blockType::CACTUS, blockType::NOTHING,
+            blockType::CACTUS, blockType::NOTHING
+    }));
+
+    structures.push_back(structure("cactus_1", 3, 7, {
+            blockType::NOTHING, blockType::CACTUS, blockType::NOTHING,
+            blockType::CACTUS, blockType::CACTUS, blockType::NOTHING,
+            blockType::CACTUS, blockType::CACTUS, blockType::CACTUS,
+            blockType::CACTUS, blockType::CACTUS, blockType::CACTUS,
+            blockType::NOTHING, blockType::CACTUS, blockType::CACTUS,
+            blockType::NOTHING, blockType::CACTUS, blockType::NOTHING,
+            blockType::NOTHING, blockType::CACTUS, blockType::NOTHING
+    }));
+
+    structures.push_back(structure("cactus_2", 2, 3, {
+            blockType::CACTUS, blockType::CACTUS,
+            blockType::CACTUS, blockType::CACTUS,
+            blockType::CACTUS, blockType::NOTHING,
     }));
     
 
     ofstream structureFile;
-    structureFile.open("C:/Users/Uporabnik/source/repos/Zorz42/Terralistic/x64/Debug/Resources/Structures.asset", ios::out | ios::trunc);
+    structureFile.open("/home/nejc/CLionProjects/Terralistic/Resources/resourcePack/misc/structures.asset", ios::out | ios::trunc);
 
     for (structure& i : structures) {
-        int size = (int)i.name.size() + i.x_size * i.y_size + 3;
+        int size = (int)i.name.size() + (i.x_size * i.y_size * 2) + 5;
         char* data = new char[size];
         data[0] = (char)i.name.size();
         for (int i2 = 0; i2 < i.name.size(); i2++)
             data[i2 + 1] = i.name[i2];
         char* arr_section = data + i.name.size() + 1;
-        arr_section[0] = i.x_size;
-        arr_section[1] = i.y_size;
-        for (int i2 = 0; i2 < i.x_size * i.y_size; i2++)
-            arr_section[i2 + 2] = (char)i.blocks[i2];
+        arr_section[0] = (char)((i.x_size >> 8) & 0xff);
+        arr_section[1] = (char)(i.x_size & 0xff);
+        arr_section[2] = (char)((i.y_size >> 8) & 0xff);
+        arr_section[3] = (char)(i.y_size & 0xff);
+        for (int i2 = 0; i2 < i.x_size * i.y_size; i2++) {
+            arr_section[i2 * 2 + 4] = (char) (((int)i.blocks[i2] >> 8) & 0xff);
+            arr_section[i2 * 2 + 5] = (char) ((int)i.blocks[i2] & 0xff);
+        }
         structureFile.write(data, size);
     }
 
     structureFile.close();
 
 
-    structureFile.open("C:/Users/Uporabnik/source/repos/Zorz42/Terralistic/Client/Resources/Structures.asset", ios::out | ios::trunc);
+    structureFile.open("/home/nejc/CLionProjects/Terralistic/cmake-build-debug/Resources/resourcePack/misc/structures.asset", ios::out | ios::trunc);
 
     for (structure& i : structures) {
-        int size = (int)i.name.size() + i.x_size * i.y_size + 3;
+        int size = (int)i.name.size() + (i.x_size * i.y_size * 2) + 5;
         char* data = new char[size];
         data[0] = (char)i.name.size();
         for (int i2 = 0; i2 < i.name.size(); i2++)
             data[i2 + 1] = i.name[i2];
         char* arr_section = data + i.name.size() + 1;
-        arr_section[0] = i.x_size;
-        arr_section[1] = i.y_size;
-        for (int i2 = 0; i2 < i.x_size * i.y_size; i2++)
-            arr_section[i2 + 2] = (char)i.blocks[i2];
+        arr_section[0] = (char)((i.x_size >> 8) & 0xff);
+        arr_section[1] = (char)(i.x_size & 0xff);
+        arr_section[2] = (char)((i.y_size >> 8) & 0xff);
+        arr_section[3] = (char)(i.y_size & 0xff);
+        for (int i2 = 0; i2 < i.x_size * i.y_size; i2++) {
+            arr_section[i2 * 2 + 4] = (char) (((int)i.blocks[i2] >> 8) & 0xff);
+            arr_section[i2 * 2 + 5] = (char) ((int)i.blocks[i2] & 0xff);
+        }
         structureFile.write(data, size);
     }
 
     structureFile.close();
+
 }
 
 
